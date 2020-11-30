9e74282eba782d129646047e63780e07acc02b86
Julius Dehner <julius.dehner@gmail.com>
2020-11-30 22:57:18 +0100

    Sending clipboard content working

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
+
+
+class Message(BaseModel):
+    content: str
 
 app = FastAPI()
 
+app.mount("/static/", StaticFiles(directory="static"), name="static")
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+messages = []
+
+@app.post("/send/")
+async def post_content(message: Message):
+    messages.append(message.content)
+    return message
+
+@app.get("/request/")
+async def get_content():
+    return messages
